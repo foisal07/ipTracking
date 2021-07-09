5906a7a67d593c37de77679743f00a095054bdbc
Foisal <workkaj01@gmail.com>
2021-07-09 13:38:37 +0600

    Add map

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,12 @@ const iptrackerForm = document.querySelector(".iptracker_form");
 const submitBtn = document.querySelector(".iptracker_form_btn");
 const iptracker_form_IP = document.querySelector(".iptracker_form_IP");
 const iptrackerDisplay = document.querySelector(".iptracker_display");
+const map = document.querySelector(".map");
 
-let ip;
-let ipInfo;
+let ip, ipInfo, lat, lng, mapView;
 
 // 118.179.125.237
+// lat: 24.374, lng: 88.60114
 
 // Get IP JSON details
 const getJSON = async function (ip) {
@@ -18,12 +19,14 @@ const getJSON = async function (ip) {
     );
     if (!res.ok) throw new Error("Invalid IP Address");
     const data = await res.json();
+    console.log(data);
     ipInfo = data;
+    lat = ipInfo.location.lat;
+    lng = ipInfo.location.lng;
   } catch (err) {
     alert(err);
   }
 };
-
 
 // Generate Display IP Details Markup
 const displayTrackedInfo = function (ipInfo) {
@@ -43,12 +46,39 @@ const displayTrackedInfo = function (ipInfo) {
         <div class="iptracker_display_box">
           <p class="iptracker_display_title">ISP</p>
           <p class="iptracker_display_info">${ipInfo.isp}</p>
-        </div>
-      </div>
-    `;
+          </div>
+          </div>
+          `;
   iptrackerDisplay.insertAdjacentHTML("beforeend", displayMarkup);
 };
 
+// Display map
+const displayMap = function (lat, lng) {
+  // Create leaflet map
+  mapView = L.map("map").setView([lat, lng], 15);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZm9pc2FsMDciLCJhIjoiY2txdnNxazRhMGhsMTJvbWg2OWlyODN6NyJ9.Tf_hHhpLFTpQPmbMl_wbSQ",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 20,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1IjoiZm9pc2FsMDciLCJhIjoiY2txdnNxazRhMGhsMTJvbWg2OWlyODN6NyJ9.Tf_hHhpLFTpQPmbMl_wbSQ",
+    }
+  ).addTo(mapView);
+
+  // Leaflet map marker
+  L.popup()
+    .setLatLng([lat, lng])
+    .setContent("Gtocha! The IP Located Around Here")
+    .openOn(mapView);
+};
+
+// Display IP details on form submit
 iptrackerForm.addEventListener("submit", async function (e) {
   e.preventDefault();
 
@@ -58,25 +88,41 @@ iptrackerForm.addEventListener("submit", async function (e) {
   // Get ip details
   await getJSON(ip);
 
+  // Clear search form & previous IP details
+  iptracker_form_IP.value = "";
+  iptrackerDisplay.textContent = "";
+
   // Display IP details
   displayTrackedInfo(ipInfo);
+
+  // Remove old map
+  mapView.remove();
+
+  // Display Map
+  displayMap(lat, lng);
 });
 
-// Detect IP & display IP details on load
-// window.addEventListener("load", async function (e) {
-//   e.preventDefault();
+// Detect & display IP details on load
+window.addEventListener("load", async function (e) {
+  e.preventDefault();
 
-//   // Get IP from form browser
-//   const res = await fetch("https://api.ipify.org/?format=json");
-//   const data = await res.json();
-//   ip = data.ip;
+  // Get IP from form browser
+  const res = await fetch("https://api.ipify.org/?format=json");
+  const data = await res.json();
+  ip = data.ip;
 
-//   // Get ip details
-//   await getJSON(ip);
+  // Get ip details
+  await getJSON(ip);
 
-//   // Display IP details
-//   displayTrackedInfo(ipInfo);
-// });
+  // Clear previous details
+  iptrackerDisplay.textContent = "";
+
+  // Display IP details
+  displayTrackedInfo(ipInfo);
+
+  // Display Map
+  displayMap(lat, lng);
+});
 
 // const controlIpTrack = async function (ip, ipInfo) {
 //   await getJSON(ip);
